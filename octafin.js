d876f7117843f9472a0ce055a9c3983d6b70dfef
Octafin <leoandrebenezit@gmail.com>
2017-06-12 17:24:52 +0100

    Fixes to contact form

--- octafin.js
+++ octafin.js
@@ -1,5 +1,27 @@
-$(document).ready(function({
-  $(".github").bounce("slow");
-})
-
+$("#contact-form").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
 });
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var tel = $("#tel").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: "php/process.php",
+        data: "name=" + name + "&tel=" + tel + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "hidden" );
+}
